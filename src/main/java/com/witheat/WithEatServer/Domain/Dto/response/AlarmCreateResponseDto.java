296103885cf7af8a2fe6cc4e6ccd7a0fed6ff98e
seoyoun <angel6462@naver.com>
2023-11-09 20:48:03 +0900

    알람 컨드롤 구현, 알람 서비스 로직 짜기

--- src/main/java/com/witheat/WithEatServer/Domain/Dto/response/AlarmCreateResponseDto.java
+++ src/main/java/com/witheat/WithEatServer/Domain/Dto/response/AlarmCreateResponseDto.java
@@ -1,5 +1,11 @@
 package com.witheat.WithEatServer.Domain.Dto.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+
 public class AlarmCreateResponseDto {
-g
+    private Long alarm_id; //alarm_id
 }
